a57a24bfa9b044afe2e89af5b91a7916d0935b99
sdemole <sdemole@gmail.com>
2014-01-14 17:51:01 -0500

    updated

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.get('/', function(req, res){
 
 app.get('/login', function(req, res){
 	res.render('login', { title: 'Login'})
-	console.log('GET request, from: '+req.connection.remoteAddress)
+	console.log('GET /login request, from: '+req.connection.remoteAddress)
 });
 
 app.post('/login', function(req, res){
